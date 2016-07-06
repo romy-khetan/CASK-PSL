1d6f30e3d0a110231c7bddb96725ffa8a19354bb
bhushankawadkar <bhushan_kawadkar@persistent.com>
2016-07-06 16:44:13 +0530

    Reverted code changes done for FileAction plugin

--- hydrator-plugins/core-plugins/src/main/java/co/cask/hydrator/plugin/common/BatchXMLFileFilter.java
+++ hydrator-plugins/core-plugins/src/main/java/co/cask/hydrator/plugin/common/BatchXMLFileFilter.java
@@ -17,32 +17,28 @@
 package co.cask.hydrator.plugin.common;
 
 import co.cask.hydrator.plugin.batch.source.XMLInputFormat;
-import co.cask.hydrator.plugin.batch.source.XMLReaderBatchSource;
+import com.google.common.base.Strings;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.PathFilter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.annotation.Nullable;
 
 /**
  * Filter class to filter out XML filenames in the input path.
  */
 public class BatchXMLFileFilter extends Configured implements PathFilter {
-
-  private static final Logger LOG = LoggerFactory.getLogger(XMLReaderBatchSource.class);
   private static final Gson GSON = new Gson();
-  private static final Type ARRAYLIST_PREPROCESSED_FILES  = new TypeToken<ArrayList<String>>() { }.getType();
+  private static final Type ARRAYLIST_PREPROCESSED_FILES = new TypeToken<ArrayList<String>>() { }.getType();
   private Pattern regex;
   private String pathName;
 
@@ -55,21 +51,20 @@ public class BatchXMLFileFilter extends Configured implements PathFilter {
     if (filePathName.equals(pathName)) {
       return true;
     }
-    boolean patternMatch = true;
-    Matcher matcher = regex.matcher(filePathName);
-    patternMatch = matcher.find();
+    Matcher matcher = regex.matcher(path.getName());
+    boolean patternMatch = matcher.find();
     if (patternMatch && CollectionUtils.isNotEmpty(preProcessedFileList)) {
-       patternMatch = !preProcessedFileList.contains(filePathName);
+      patternMatch = !preProcessedFileList.contains(filePathName);
     }
     return patternMatch;
   }
 
   @Override
-  public void setConf(Configuration conf) {
+  public void setConf(@Nullable Configuration conf) {
     if (conf == null) {
       return;
     }
-    pathName = conf.get(XMLInputFormat.XML_INPUT_NAME_CONFIG, "/");
+    pathName = conf.get(XMLInputFormat.XML_INPUTFORMAT_PATH_NAME, "/");
 
     //path is a directory so remove trailing '/'
     if (pathName.endsWith("/")) {
@@ -79,8 +74,8 @@ public class BatchXMLFileFilter extends Configured implements PathFilter {
     String input = conf.get(XMLInputFormat.XML_INPUTFORMAT_PATTERN, ".*");
     regex = Pattern.compile(input);
 
-    String processedFiles = conf.get(XMLInputFormat.XML_INPUTFORMAT_PROCESSED_FILES, "");
-    if (StringUtils.isNotEmpty(processedFiles)) {
+    String processedFiles = conf.get(XMLInputFormat.XML_INPUTFORMAT_PROCESSED_FILES);
+    if (!Strings.isNullOrEmpty(processedFiles)) {
       preProcessedFileList = GSON.fromJson(processedFiles, ARRAYLIST_PREPROCESSED_FILES);
     }
   }
